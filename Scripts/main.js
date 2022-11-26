72552b80c077565d93010ff1943bc0a96b46fbce
Felix Krause <contact@flyx.org>
2022-11-26 11:51:29 +0100

    Extracted nova-extension-utils to own Flake
     * see github.com/flyx/nova-extension-utils, which is now
       used as Flake input
     * Setting keys have changed, may need to enter values again

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,89 +1,38 @@
-let langserver = null;
+const LanguageServer = require("language-server.js").LanguageServer;
+const Config = require("config.js").Config;
+const Formatter = require("formatter.js").Formatter;
+
+let config = null;
 let taskprovider = null;
 
 exports.activate = () => {
-  console.log("Activating Zig Extension");
-  langserver = new ZigLanguageServer();
+  if (nova.inDevMode()) console.log("Activating Zig Extension");
+  config = new Config();
+  nova.subscriptions.add(new LanguageServer(
+    "org.flyx.zig.client", {
+      syntaxes: ["zig"]
+    }, config.langServer.path
+  ));
+  nova.subscriptions.add(new Formatter(
+    "org.flyx.zig.command.fmt",
+    config.compiler.path, [ "fmt", "--stdin" ],
+    [ "zig" ],
+    config.compiler.formatOnSave
+  ));
   taskprovider = new ZigTaskProvider();
-  nova.commands.register(
-    ZigFormatter.identifier, ZigFormatter.formatDocument
-  );
-  nova.workspace.onDidAddTextEditor((editor) => {
-    editor.onWillSave(async (textEditor) => {
-      if (getConfig("org.flyx.zig.fmt")) {
-        await nova.commands.invoke(ZigFormatter.identifier, textEditor);
-      }
-    });
-  });
 }
 
 exports.deactivate = () => {
-  console.log("Deactivating Zig Extension");
-  langserver.deactivate();
-  langserver = null;
-}
-
-function getConfig(id) {
-  let res = nova.workspace.config.get(id);
-  if (res == null) res = nova.config.get(id + ".default");
-  return res;
-}
-
-class ZigFormatter {
-  static identifier = "org.flyx.zig.command.fmt";
-  
-  static formatDocument(a, b) {
-    const editor = b ? b : a;
-    const doc = editor.document;
-    if (doc.syntax == "zig") {
-      console.log("attempting formatting");
-      
-      const formatter = new Process("/usr/bin/env", {
-        cwd: nova.workspace.path,
-        stdio: "pipe",
-        args: [
-          getConfig("org.flyx.zig.path"),
-          "fmt",
-          "--stdin"
-        ],
-      });
-      formatter.onStderr((line) => console.error(`[zig fmt] ${line}`));
-      
-      const all = new Range(0, doc.length);
-      
-      return new Promise((resolve, reject) => {
-        let result = "";
-        formatter.onStdout((line) => result += line);
-        formatter.onDidExit((status) => {
-          if (status == 0) {
-            editor.edit((edit) => {
-              edit.replace(all, result);
-            }).then(() => resolve());
-          } else (reject());
-        });
-        
-        const writer = formatter.stdin.getWriter();
-        writer.ready.then(() => {
-          writer.write(doc.getTextInRange(all));
-          console.log("finished writing");
-          writer.close();
-        });
-        
-        console.log("starting formatting");
-        formatter.start();
-      });
-    }
-  }
+  if (nova.inDevMode()) console.log("Deactivating Zig Extension");
 }
 
 class ZigTaskProvider {
   static identifier = "org.flyx.zig.tasks";
-  static config     = "org.flyx.zig.path";
   
   static zigBuild(...options) {
     return new TaskProcessAction("/usr/bin/env", {
       args: [
-        getConfig("org.flyx.zig.path"),
+        config.compiler.path.value(),
         "build",
         ...options
       ]
@@ -110,15 +59,14 @@ class ZigTaskProvider {
       name: "Zig Build",
     });
     nova.fs.watch("build.zig", () => this.reload());
-    nova.workspace.config.onDidChange(ZigTaskProvider.config, () => this.reload());
-    nova.config.onDidChange(ZigTaskProvider.config + ".default", () => this.reload());
+    config.compiler.path.onDidChange(() => this.reload());
     this.reload();
   }
   
   reload() {
     let tasks = [];
     const buildZig = ZigTaskProvider.findBuildZig();
-    const zigPath = getConfig("org.flyx.zig.path");
+    const zigPath = config.compiler.path.value();
     if (buildZig != null && zigPath != null) {
       const query = new Process("/usr/bin/env", {
         cwd: nova.workspace.path,
@@ -144,15 +92,14 @@ class ZigTaskProvider {
         }
       });
       query.onStderr((line) => {
-        console.error(`[zig build -h] ${line}`);
+        console.error(`[reading build.zig] ${line}`);
       });
       query.onDidExit((status) => {
-        if (status == 0) {
-          this.tasks = tasks;
-          nova.workspace.reloadTasks(ZigTaskProvider.identifier);
-        } else {
-          console.log("`zig build -h` exited with non-zero status!");
+        if (status != 0) {
+          console.log("`[reading build.zig] zig build -h` exited with non-zero status!");
         }
+        this.tasks = tasks;
+        nova.workspace.reloadTasks(ZigTaskProvider.identifier);
       });
       query.start();
     } else {
@@ -165,78 +112,5 @@ class ZigTaskProvider {
   
   provideTasks() {
     return this.tasks;
-  }
-}
-
-class ZigLanguageServer {
-  static identifier = "org.flyx.zig.zls.server";
-  static config     = "org.flyx.zig.zls.path";
-  
-  constructor() {
-    // Observe the configuration setting for the server's location, and restart the server on change
-    nova.config.onDidChange(ZigLanguageServer.config, function(path) {
-      this.start();
-    }, this);
-    nova.config.onDidChange(ZigLanguageServer.config + ".default", function(path) {
-      this.start();
-    }, this);
-    this.start();
-  }
-    
-  deactivate() {
-    this.stop();
-  }
-    
-  start() {
-    this.stop();
-    const path = getConfig(ZigLanguageServer.config);
-    
-    const proc = new Process("/usr/bin/which", {
-      args: [path]
-    });
-    let abs_path = null;
-    proc.onStdout((line) => {
-      abs_path = line.trim();
-    });
-    proc.onDidExit((status) => {
-      if (status == 0) {
-        var serverOptions = {
-          path: abs_path,
-          args: [],
-        };
-        var clientOptions = {
-          // The set of document syntaxes for which the server is valid
-          syntaxes: ["zig"]
-        };
-        var client = new LanguageClient(
-          `org.flyx.zig.zls`,
-          path,
-          serverOptions,
-          clientOptions
-        );
-        
-        try {
-          client.start();
-          if (nova.inDevMode()) console.log(`started language server: zig`);
-          
-          // Add the client to the subscriptions to be cleaned up
-          nova.subscriptions.add(client);
-          this.languageClient = client;
-        } catch (err) {
-          console.error(`while trying to start ${abs_path}:`, err)
-        }
-      } else {
-        console.warn(`unable to find language server in PATH:`, path);
-      }
-    });
-    proc.start();
-  }
-    
-  stop() {
-    if (this.languageClient) {
-      this.languageClient.stop();
-      nova.subscriptions.remove(this.languageClient);
-      this.languageClient = null;
-    }
   }
 }
--- flake.nix
+++ flake.nix
@@ -1,144 +1,99 @@
 {
   inputs = {
-    nixpkgs.url     = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nova-utils.url = "github:flyx/nova-extension-utils";
     tree-sitter-zig = {
-      url   = github:maxxnino/tree-sitter-zig;
+      url = "github:maxxnino/tree-sitter-zig";
       flake = false;
     };
-    utils.url = github:numtide/flake-utils;
-    nova      = {
-      url   = "https://download.panic.com/nova/Nova%2010.zip";
-      flake = false;
-    };
+    utils.url = "github:numtide/flake-utils";
     logo = {
-      url   = github:ziglang/logo;
+      url = "github:ziglang/logo";
       flake = false;
     };
   };
-  outputs = {
-    self, nixpkgs, tree-sitter-zig, utils, nova, logo
-  }: utils.lib.eachSystem [
-    utils.lib.system.x86_64-darwin
-    utils.lib.system.aarch64-darwin
-  ] (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-    syntax-lib = pkgs.stdenvNoCC.mkDerivation {
-      name       = "tree-sitter-zig-dylib";
-      src        = tree-sitter-zig;
-      buildPhase = ''
-        FRAMEWORKS_PATH="${nova}/Contents/Frameworks/"
-        BUILD_FLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=11.0 -Isrc -Wall -Wextra"
-        LDFLAGS="$BUILD_FLAGS -F$FRAMEWORKS_PATH -framework SyntaxKit -rpath @loader_path/../Frameworks"
-        LINKSHARED="-dynamiclib -Wl,-install_name,libtree-sitter-zig.dylib,-rpath,@executable_path/../Frameworks"
-        /usr/bin/clang -c $BUILD_FLAGS -o parser.o src/parser.c
-        echo "linking"
-        /usr/bin/clang $LDFLAGS $LINKSHARED parser.o -o libtree-sitter-zig.dylib
-        /usr/bin/codesign -s - libtree-sitter-zig.dylib
-      '';
-      installPhase = ''
-        cp libtree-sitter-zig.dylib $out
-      '';
-    };
-    
-    version = "0.1.0";
-    meta    = {
-      description = "Zig language support";
-      homepage    = "https://github.com/flyx";
-      license     = nixpkgs.lib.licenses.mit;
-      maintainers = [ "Felix Krause" ];
-    };
-    cfgSection = {basePath, description, items, ...}@args: isWorkspace: {
-      children = builtins.map (
-        item: {
-          key = basePath + "." + item.id + (if isWorkspace then "" else ".default");
-        } // (builtins.removeAttrs item [ "id" "default" ]) // (if isWorkspace then {} else {
-          inherit (item) default;
-        })
-      ) items;
-      description = if isWorkspace then
-        (description + " (if set, overrides default)")
-      else (description + " (may be overridden per workspace)");
-      type = "section";
-    } // (builtins.removeAttrs args [ "basePath" "items" ]);
-    configItems = isWorkspace: [
-      (cfgSection {
-        basePath    = "org.flyx.zig";
-        description = "Settings for the Zig compiler";
-        items       = [
-          {
-            id      = "path";
-            title   = "Path to the Zig compiler";
-            type    = "path";
-            default = "zig";
-          }
-          {
-            id      = "fmt";
-            title   = "Format on Save";
-            type    = "boolean";
-            default = false;
-          }
-        ];
-      } isWorkspace)
-      (cfgSection {
-        basePath    = "org.flyx.zig.zls";
-        description = "Settings for the ZLS lanugage server";
-        items       = [
-          {
-            id      = "path";
-            title   = "Path to ZLS";
-            type    = "path";
-            default = "zls";
-          }
-        ];
-      } isWorkspace)
-    ];
-  in {
-    defaultPackage = pkgs.stdenv.mkDerivation {
-      pname   = "Zig.novaextension";
-      version = "0.1.0";
-      src     = self;
-      JSON    = builtins.toJSON {
-        inherit version;
-        identifier       = "org.flyx.Zig";
-        name             = "zig";
-        organization     = "Felix Krause";
-        description      = meta.description;
-        categories       = [ "languages" "commands" "completions" ];
-        config           = configItems false;
-        configWorkspace  = configItems true;
-        main             = "main.js";
-        bugs             = "https://github.com/flyx/NovaZig/issues";
-        repository       = "https://github.com/flyx/NovaZig";
-        funding          = "https://github.com/sponsors/flyx";
-        license          = "MIT";
-        activationEvents = [
-          "onLanguage:zig"
-          "onWorkspaceContains:*.zig"
-        ];
-        entitlements = {
-          process    = true;
-          filesystem = "readonly";
+  outputs = { self, nixpkgs, nova-utils, tree-sitter-zig, utils, logo }:
+    utils.lib.eachSystem [
+      utils.lib.system.x86_64-darwin
+      utils.lib.system.aarch64-darwin
+    ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nova-utils.overlays.default ];
         };
-        commands.editor = [
-          {
-            title    = "Format Zig File";
-            command  = "org.flyx.zig.command.fmt";
-            when     = "editorHasFocus && editorSyntax == 'zig'";
+        syntax-lib = pkgs.buildNovaTreeSitterLib {
+          langName = "zig";
+          src = tree-sitter-zig;
+        };
+      in {
+        packages.default = pkgs.buildNovaExtension {
+          name = "zig";
+          version = "0.1.1";
+          src = self;
+          identifier = "org.flyx.zig";
+          organization = "Felix Krause";
+          description = "Zig language support";
+          categories = [ "languages" "commands" "completions" ];
+          license = "MIT";
+          activationEvents = [ "onLanguage:zig" "onWorkspaceContains:*.zig" ];
+          bugs = "https://github.com/flyx/NovaZig/issues";
+          repository = "https://github.com/flyx/NovaZig";
+          funding = "https://github.com/sponsors/flyx";
+          treeSitterLibs = [ syntax-lib ];
+          derivationParams = {
+            buildInputs = with pkgs; [ gnused librsvg ];
+            postInstall = ''
+              ${pkgs.gnused}/bin/sed 's/@fold/@subtree/g' ${tree-sitter-zig}/queries/folds.scm >$extDir/Queries/folds.scm
+              ${pkgs.librsvg}/bin/rsvg-convert -w 32 -h 32 ${logo}/zig-mark.svg -o $extDir/extension.png
+              ${pkgs.librsvg}/bin/rsvg-convert -w 64 -h 64 ${logo}/zig-mark.svg -o $extDir/extension@2x.png
+            '';
+          };
+          config = [
+            {
+              name = "compiler";
+              title = "Settings for the Zig compiler";
+              type = "section";
+              children = [
+                {
+                  name = "path";
+                  title = "Path to the Zig compiler";
+                  type = "path";
+                  default = "zig";
+                }
+                {
+                  name = "formatOnSave";
+                  title = "Format on Save";
+                  type = "boolean";
+                  default = false;
+                }
+              ];
+            }
+            {
+              name = "langServer";
+              title = "Settings for the ZLS lanugage server";
+              type = "section";
+              children = [
+                {
+                  name = "path";
+                  title = "Path to ZLS";
+                  type = "path";
+                  default = "zls";
+                }
+              ];
+            }
+          ];
+          main = "main.js";
+          entitlements = {
+            process = true;
+            filesystem = "readonly";
+          };
+          commands.editor = [{
+            title = "Format Zig File";
+            command = "org.flyx.zig.command.fmt";
+            when = "editorHasFocus && editorSyntax == 'zig'";
             shortcut = "opt-shift-F";
-          }
-        ];
-      };
-      buildInputs = with pkgs; [ gnused librsvg ];
-    installPhase = ''
-      mkdir -p $out/Zig.novaextension/Queries
-      cp -r -t $out/Zig.novaextension Completions Scripts Syntaxes Queries CHANGELOG.md
-      cp README-user.md $out/Zig.novaextension/README.md
-      cp ${syntax-lib} $out/Zig.novaextension/Syntaxes/libtree-sitter-zig.dylib
-      printenv JSON >$out/Zig.novaextension/extension.json
-      ${pkgs.gnused}/bin/sed 's/@fold/@subtree/g' ${tree-sitter-zig}/queries/folds.scm >$out/Zig.novaextension/Queries/folds.scm
-      ${pkgs.librsvg}/bin/rsvg-convert -w 16 -h 16 ${logo}/zig-mark.svg -o $out/Zig.novaextension/extension.png
-      ${pkgs.librsvg}/bin/rsvg-convert -w 32 -h 32 ${logo}/zig-mark.svg -o $out/Zig.novaextension/extension@2x.png
-    '';
-    };
-  });
+          }];
+        };
+      });
 }
